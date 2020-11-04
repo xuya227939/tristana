96a4b1b772eef981d76397947a2a16362a01137c
发声的沉默者 <hezhiqianye@gmail.com>
2020-11-04 09:53:11 +0800

    Update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -12,13 +12,13 @@ module.exports = {
     // 对象的 key 仅在必要时用引号
     quoteProps: 'as-needed',
     // jsx 不使用单引号，而使用双引号
-    jsxSingleQuote: true,
+    jsxSingleQuote: false,
     // 末尾不需要逗号
-    trailingComma: true,
+    trailingComma: 'none',
     // 大括号内的首尾需要空格
     bracketSpacing: true,
     // jsx 标签的反尖括号需要换行
-    jsxBracketSameLine: true,
+    jsxBracketSameLine: false,
     // 箭头函数，只有一个参数的时候，也需要括号
     arrowParens: 'avoid',
     // 每个文件格式化的范围是文件的全部内容
