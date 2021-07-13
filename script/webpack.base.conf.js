d977488b528df14a2978c149ba02d0d4d61da916
发声的沉默者 <hezhiqianye@gmail.com>
2021-07-13 16:35:37 +0800

    Update webpack.base.conf.js

--- script/webpack.base.conf.js
+++ script/webpack.base.conf.js
@@ -199,7 +199,10 @@ module.exports = {
         ]),
         HotModuleReplacementPlugin: new webpack.HotModuleReplacementPlugin(),
         ReactRefreshWebpackPlugin: new ReactRefreshWebpackPlugin(),
-        HardSourceWebpackPlugin: new HardSourceWebpackPlugin()
+        HardSourceWebpackPlugin: new HardSourceWebpackPlugin(),
+        providePlugin: new webpack.ProvidePlugin({
+            React: 'React'
+        })
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
