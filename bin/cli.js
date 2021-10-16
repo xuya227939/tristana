be0c8a11e5e9ed1593b3d8ac8f345cfb4b46cf51
xuya227939 <hezhiqianye@gmail.com>
2021-10-16 12:06:27 +0800

    feat: 完成 cli 脚手架搭建

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,6 @@
 
 const program = require("commander");
 const chalk = require("chalk");
-// const ora = require("ora");
 const figlet = require("figlet");
 
 program
@@ -14,37 +13,14 @@ program
   .usage("<command> [option]")
   .action((name, options) => {
     require("../lib/create.js")(name, options);
-    // console.log(
-    //   "Tristana " + chalk.green(`v${require("../package.json").version}`)
-    // );
-    // console.log();
-    // // 打印命令行输入的值
-    // console.log(chalk.green("Tristana即将创建一个新项目 !"));
-    // console.log(
-    //   "Need Help? Go and open issue: https://github.com/xuya227939/tristana/issues/new"
-    // );
   });
 
-program
-  // 监听 --help 执行
-  .on("--help", () => {
-    // 使用 figlet 绘制 Logo
-    console.log(
-      "\r\n" +
-        // figlet.textSync("React", {
-        //   font: "Ghost",
-        //   horizontalLayout: "default",
-        //   verticalLayout: "default",
-        //   width: 80,
-        //   whitespaceBreak: true,
-        // })
-        figlet.textSync("React")
-    );
-    // 新增说明信息
-    console.log(
-      `\r\nRun ${chalk.cyan(`roc <command> --help`)} show details\r\n`
-    );
-  });
+program.on("--help", () => {
+  // 使用 figlet 绘制 Logo
+  console.log("\r\n" + figlet.textSync("React"));
+  // 新增说明信息
+  console.log(`\r\nRun ${chalk.cyan(`roc <command> --help`)} show details\r\n`);
+});
 
 // program
 //   .command("-v")
