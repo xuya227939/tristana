aa9a123c7560b86e629a3cd084784704430fd56e
江辰 <hezhiqianye@gmail.com>
2022-03-30 17:44:57 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './app';
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
