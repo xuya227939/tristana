b8ab4d404c809c5a37c921856d4db42f990d715a
xuya227939 <hezhiqianye@gmail.com>
2023-04-09 19:13:27 +0800

    fix: fix assets path error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-const path = require('path');
 export default defineConfig({
     server: {
         port: 3000
@@ -19,6 +19,7 @@ export default defineConfig({
             '@mock': path.resolve(__dirname, 'src/mock')
         }
     },
+    assetsInclude: ['**/*.jpg'],
     plugins: [react()],
     css: {
         preprocessorOptions: {
